1adc1c07b2a7538a6473bc7d958debebe48ec960
Sutirtho Sen <sutirtho.sen.90@gmail.com>
2017-07-01 06:28:20 -0700

    Login for system users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def home
+    unless logged_in?
+      redirect_to '/login'
+    end
+  end
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to '/login'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get '/', to: 'sessions#home'
   resources :users
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
