25102604cabe6c1e1cf63137d2dc63ecb0ca5d99
Sutirtho Sen <sutirtho.sen.90@gmail.com>
2017-07-02 00:29:27 -0700

    Adding signature and logo upload for companies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,20 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+
+  def upload_file (upoloadedFileObj)
+    originalFileName = upoloadedFileObj.original_filename
+    finalFileName = Time.now.to_i.to_s + Random.rand(100).to_s + originalFileName
+    File.open(Rails.root.join('public', 'uploads', finalFileName), 'wb') do |file|
+      file.write(upoloadedFileObj.read)
+    end
+    return File.join('/', 'uploads', finalFileName)
+  end
+
+  def actual_path (uploadPath)
+    if uploadPath[0] == '/'
+      uploadPath = uploadPath[1..-1]
+    end
+    return Rails.root.join('public',uploadPath)
+  end
 end
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
   before_action :logged_in_user
@@ -25,8 +26,16 @@ class CompaniesController < ApplicationController
   # POST /companies
   # POST /companies.json
   def create
-    @company = Company.new(company_params)
+    allowed_params = company_params
+    if (allowed_params[:authorized_sign_img] and allowed_params[:authorized_sign_img] != '')
+      allowed_params[:authorized_sign_img] = upload_file allowed_params[:authorized_sign_img]
+    end
 
+    if (allowed_params[:company_logo_img] and allowed_params[:company_logo_img] != '')
+      allowed_params[:company_logo_img] = upload_file allowed_params[:company_logo_img]
+    end
+
+    @company = Company.new(allowed_params)
     respond_to do |format|
       if @company.save
         format.html { redirect_to @company, notice: 'Company was successfully created.' }
@@ -41,8 +50,23 @@ class CompaniesController < ApplicationController
   # PATCH/PUT /companies/1
   # PATCH/PUT /companies/1.json
   def update
+    allowed_params = company_params
+    if (allowed_params[:authorized_sign_img] and allowed_params[:authorized_sign_img] != '')
+      if(@company.authorized_sign_img and @company.authorized_sign_img != '' and File.exists?(actual_path @company.authorized_sign_img))
+        FileUtils.rm(actual_path @company.authorized_sign_img)
+      end
+      allowed_params[:authorized_sign_img] = upload_file allowed_params[:authorized_sign_img]
+    end
+
+    if (allowed_params[:company_logo_img] and allowed_params[:company_logo_img] != '')
+      if(@company.company_logo_img and @company.company_logo_img != '' and File.exists?(actual_path @company.company_logo_img))
+        FileUtils.rm(actual_path @company.company_logo_img)
+      end
+      allowed_params[:company_logo_img] = upload_file allowed_params[:company_logo_img]
+    end
+
     respond_to do |format|
-      if @company.update(company_params)
+      if @company.update(allowed_params)
         format.html { redirect_to @company, notice: 'Company was successfully updated.' }
         format.json { render :show, status: :ok, location: @company }
       else
@@ -55,6 +79,12 @@ class CompaniesController < ApplicationController
   # DELETE /companies/1
   # DELETE /companies/1.json
   def destroy
+    if(@company.authorized_sign_img)
+      FileUtils.rm(actual_path @company.authorized_sign_img)
+    end
+    if(@company.company_logo_img)
+      FileUtils.rm(actual_path @company.company_logo_img)
+    end
     @company.destroy
     respond_to do |format|
       format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
