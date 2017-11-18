2d78666aa713db6c9a620ffdf82865d0ce3e1b6d
Sutirtho <sutirtho.sen.90@gmail.com>
2017-11-18 22:49:09 +0530

    Updated sidebar behavior and added ability to create tax types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,20 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  def title_match(title, matcher)
+    if matcher.kind_of?(Array)
+      matcher.each do |mt|
+        if /#{mt}/.match(title)
+          return true
+        end
+      end
+      return false
+    else
+      if /#{matcher}/.match(title)
+        return true
+      end
+      return false
+    end
+  end
 end
--- app/models/tax.rb
+++ app/models/tax.rb
@@ -1,5 +1,4 @@
 class Tax < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :perc
-  validates_presence_of :is_active
 end
